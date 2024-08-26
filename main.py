c9944b64d74acb19e2916cfaf6527535ff145b51
eadrom <eadrom@protonmail.com>
2024-08-26 11:22:25 -0700

    added player.py, intialize player ship

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
@@ -12,11 +13,14 @@ def main():
     game_clock = pygame.time.Clock()
     dt = 0
 
+    player_ship = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
+        player_ship.draw(screen)
         pygame.display.flip()
         dt = game_clock.tick(60) / 1000
 
